4102961c7c92979216e4706b815f9a266c199de1
Yuan Chong <ychongsaytc@gmail.com>
2017-02-21 15:30:08 +0800

    unified signing validation directory

--- sign-batch.sample.sh
+++ sign-batch.sample.sh
@@ -3,14 +3,19 @@
 working_dir="`dirname $0`"
 
 "$working_dir"'/sign.sh' \
+    'ychong_com' \
+    '/C=CN/ST=Beijing/L=Beijing/O=Neverland/OU=IT Dept/CN=ychong.com' \
+    '/var/www/html/default/public' \
+    'DNS:ychong.com,DNS:www.ychong.com,DNS:blog.ychong.com'
+
+"$working_dir"'/sign.sh' \
     'chon_io' \
     '/C=CN/ST=Beijing/L=Beijing/O=Neverland/OU=IT Dept/CN=chon.io' \
-    '/var/www/chon_io/public' \
+    '/var/www/html/default/public' \
     'DNS:chon.io,DNS:www.chon.io,DNS:res.chon.io'
 
 "$working_dir"'/sign.sh' \
-    'ychong_com' \
-    '/C=CN/ST=Beijing/L=Beijing/O=Neverland/OU=IT Dept/CN=ychong.com' \
-    '/var/www/chon_io/public' \
-    'DNS:ychong.com,DNS:www.ychong.com,DNS:blog.ychong.com'
+    'res_chon_io' \
+    '/C=CN/ST=Beijing/L=Beijing/O=Neverland/OU=IT Dept/CN=res.chon.io' \
+    '/var/www/html/default/public'
 
